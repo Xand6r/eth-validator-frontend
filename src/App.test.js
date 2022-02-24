172ccd34f2a0156d8f52666c4ce1faef68103901
xander <shuaibualexander@gmail.com>
2022-02-24 22:07:24 +0100

    refactor tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,16 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('Testing the Home page component', () => {
-  test('It should render a heading', () => {
+  beforeEach(() => {
     render(<App />);
+  });
+
+  test('It should render a heading', () => {
     const headingElement = screen.getByTestId('header');
     expect(headingElement).toBeInTheDocument();
   });
 
   test('It Should renders a subheading', () => {
-    render(<App />);
     const subHeadingElement = screen.getByText(
       /Please input your ethereum public address, in order to ensure it is a valid ethereum public address./i
     );
@@ -17,7 +19,6 @@ describe('Testing the Home page component', () => {
   });
 
   test('It should render a search component wrapper', () => {
-    render(<App />);
     const headingElement = screen.getByTestId('searchwrapper');
     expect(headingElement).toBeInTheDocument();
   });
--- src/components/searchComponent/__tests__/searchComponent.test.js
+++ src/components/searchComponent/__tests__/searchComponent.test.js
@@ -2,20 +2,22 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import SearchComponent, { ETH_ZERO_ADDRESS } from '../index';
 
 describe('Testing The search component', () => {
-  test('It should render an input element', () => {
+
+  beforeEach(() => {
     render(<SearchComponent />);
+  });
+
+  test('It should render an input element', () => {
     const inputElement = screen.getByPlaceholderText(`e.g ${ETH_ZERO_ADDRESS}`);
     expect(inputElement).toBeInTheDocument();
   });
 
   test('It should render a submit button', () => {
-    render(<SearchComponent />);
     const buttonElement = screen.getByRole('button');
     expect(buttonElement).toBeInTheDocument();
   });
 
   test('It should be able to type in input', () => {
-    render(<SearchComponent />);
     const SAMPLE_ADDRESS = '0x12345678';
     const inputElement = screen.getByPlaceholderText(`e.g ${ETH_ZERO_ADDRESS}`);
     fireEvent.change(inputElement, { target: { value: SAMPLE_ADDRESS } });
